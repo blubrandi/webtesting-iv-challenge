c9ee95f32e0219e70a44195a78c37a55e0fed864
blubrandi <blubrandi0@gmail.com>
2019-05-16 15:38:31 -0600

    Added server info to server.js

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/music');
+
+// sanity check route
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'This thing works!  OOOHRAH!' });
+});
+
+module.exports = server;
