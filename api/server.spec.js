53eef7cce2da87dd347dd165d79062138cb9fc1f
blubrandi <blubrandi0@gmail.com>
2019-05-17 08:31:12 -0600

    Added tests to server.spec.js.  With all passing and failing as needed.  MVP complete.

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,9 @@
 const request = require('supertest')
 const server = require('./server.js')
 
+const Songs = require('../music/music.js')
+const db = require('../data/dbConfig.js')
+
 describe('Server', () => {
     describe('GET /songs', () => {
         it('Should receive 200 res', () => {
@@ -19,5 +22,31 @@ describe('Server', () => {
                 expect(res.type).toBe('application/json')
                 done()
             })
+    })
+
+    describe('Music JS', () => {
+
+        beforeEach(async () => {
+            await db('music').truncate()
+        })
+
+        describe('add()', () => {
+            it('should add provided song', async () => {
+                await Songs.add({
+                    songName: 'Imagine',
+                    artistName: 'Beatles',
+                    songNotes: 'Imagine all the people...'
+                })
+            })
+        })
+
+        describe('remove()', () => {
+            it('should remove the song by id', () => {
+                return request(server)
+                    .delete('/songs/1')
+                    .expect(200)
+            })
+        })
+
     })
 })
